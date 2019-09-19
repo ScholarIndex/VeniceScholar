ceae9efbd031dfb294b2c7aaf3ea741b1036b40a
Vincent Barbay <vbarbay@bitbucket.org>
2019-09-19 10:30:17 +0200

    - diverses corrections (mail matteo 17.09.2019)

---
+++ config/vs.php
@@ -0,0 +1,9 @@
+<?php
+
+
+	return [
+		'JSROOT' => 'linkedbooks_refactored.',
+		'API_ROOT' => 'http://api.venicescholar.eu/v1',
+		'SOLR_CORE' => 'solr/lbcatalogue',
+		'LBC_HOST' => env('LBC_HOST'),
+	];
